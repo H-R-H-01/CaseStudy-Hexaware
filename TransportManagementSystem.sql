2207dd6b7bf1cb2aa4b970e56d69f147cb9accb0
Harish <137591297+H-R-H-01@users.noreply.github.com>
2024-11-11 07:43:50 +0530

    Update TransportManagementSystem.sql

--- TransportManagementSystem.sql
+++ TransportManagementSystem.sql
@@ -1,22 +1,22 @@
 --Case Study - Transport Management System
 
 --creating a DB
-create database TransportManagementSystem
+CREATE DATABASE TransportManagementSystem
   
 --use DB
-use TransportManagementSystem
+USE TransportManagementSystem
 
 --creating a Vehicles Table
-create table Vehicles(
+CREATE TABLE Vehicles(
 VehicleID INT IDENTITY(1,1) PRIMARY KEY,
 Model VARCHAR(255),
 Capacity DECIMAL(10,2),
-Type VARCHAR(50),
-Status VARCHAR(50)
+V_Type VARCHAR(50),
+V_Status VARCHAR(50)
 )
 
 --creating a Routes Table
-create table Routes(
+CREATE TABLE Routes(
 RouteID INT IDENTITY(1,1) PRIMARY KEY,
 StartDestination VARCHAR(255),
 EndDestination VARCHAR(255),
@@ -24,19 +24,19 @@ Distance DECIMAL(10, 2)
 )
 
 --creating a Trips Table
-create table Trips(
+CREATE TABLE Trips(
 TripID INT IDENTITY(1,1) PRIMARY KEY,
 VehicleID INT FOREIGN KEY REFERENCES Vehicles(VehicleID),
 RouteID INT FOREIGN KEY REFERENCES Routess(RouteID),
 DepartureDate DATETIME,
 ArrivalDate DATETIME,
-Status VARCHAR(50),
+V_Status VARCHAR(50),
 TripType VARCHAR(50) DEFAULT 'Freight',
 MaxPassengers INT
 )
 
 --creating passenger table
-create table Passengers(
+CREATE TABLE Passengers(
 PassengerID INT IDENTITY(1,1) PRIMARY KEY,
 FirstName VARCHAR(255),
 Gender VARCHAR(255),
@@ -46,16 +46,16 @@ PhoneNumber VARCHAR(50)
 )
   
 --creating bookings table
-create table Bookings(
+CREATE TABLE Bookings(
 BookingId int identity PRIMARY KEY,
 TripID int FOREIGN KEY REFERENCES Trips (TripID),
 PassengerID INT FOREIGN KEY REFERENCES Passengers (PassengerID),
 BookingDate DATETIME,
-[status] VARCHAR (50)
+B_Status VARCHAR (50)
 )
 
 -- Inserting sample data into the Vehicles table
-INSERT INTO Vehicles (Model, Capacity, Type, Status)
+INSERT INTO Vehicles (Model, Capacity, V_Type, V_Status)
 VALUES
 ('Ford Transit', 1.50, 'Van', 'Available'),
 ('Mercedes Actros', 18.00, 'Truck', 'On Trip'),
@@ -69,7 +69,7 @@ VALUES
 ('Chicago', 'Detroit', 280.00)
 
 -- Inserting sample data into the Trips table
-INSERT INTO Trips (VehicleID, RouteID, DepartureDate, ArrivalDate, Status, TripType, MaxPassengers)
+INSERT INTO Trips (VehicleID, RouteID, DepartureDate, ArrivalDate, V_Status, TripType, MaxPassengers)
 VALUES
 (1, 1, '2024-11-15 08:00:00', '2024-11-15 12:00:00', 'Scheduled', 'Passenger', 12),
 (2, 2, '2024-11-16 09:00:00', '2024-11-16 17:30:00', 'Scheduled', 'Freight', NULL),
@@ -83,7 +83,7 @@ VALUES
 ('Alex Brown', 'Non-binary', 22, 'alexbrown@example.com', '345-678-9012')
 
 -- Inserting sample data into the Bookings table
-INSERT INTO Bookings (TripID, PassengerID, BookingDate, Status)
+INSERT INTO Bookings (TripID, PassengerID, BookingDate, B_Status)
 VALUES
 (1, 1, '2024-11-14 15:00:00', 'Confirmed'),
 (1, 2, '2024-11-14 15:05:00', 'Confirmed'),
